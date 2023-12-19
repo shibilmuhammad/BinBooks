1809f3ea4faaf33bf02f21826ad02729bf1bd229
Shibil Muhammad <shibilmuhammad678@gmail.com>
2023-12-19 11:44:06 +0530

    commit before changing route

--- App.js
+++ App.js
@@ -3,24 +3,14 @@ const app = express();
 const path = require('path')
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')))
-const livereload = require('livereload');
-const connectLivereload = require('connect-livereload');
-const liveReloadServer = livereload.createServer(  );
-liveReloadServer.watch(path.join(__dirname, "views"));
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 10);
-});
-app.use(connectLivereload());
 app.listen(3000,()=>{
     console.log("listening to port 3000")
 })
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const userRoutes = require('./routes/user');
-app.use('/user',userRoutes);
+
+
 // Admin Routes
 const adminProducts = require('./routes/admin/products');
 const adminCustomers = require('./routes/admin/customers');
